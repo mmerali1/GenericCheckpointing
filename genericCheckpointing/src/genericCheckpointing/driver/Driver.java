bab0fb046c48c4776213ec4cb527712a08078ab2
Mehdi Merali <mmerali1@cs.binghamton.edu>
2017-12-06 14:15:54 -0500

    Added command line validation to Driver class

--- genericCheckpointing/src/genericCheckpointing/driver/Driver.java
+++ genericCheckpointing/src/genericCheckpointing/driver/Driver.java
@@ -15,73 +15,99 @@ import java.util.ArrayList;
 // import the other types used in this file
 
 public class Driver {
+	//Command line validation
+  public static void main(String[] args) {
+		boolean readFlag = true;
+		String parsArg = "";
+		int NUM_OF_OBJECTS = 0;	
+		for(int i=0; i<args.length; i++){
+			parsArg = args[i];
+			if (parsArg.charAt(0) == '$'){
+				readFlag = false;
+				break;
+			}
+			if (0 == i){
+				if ((parsArg.equals("serdeser")==false) || (parsArg.equals("deser")==false)) readFlag = false; break;
+			}
+			if (1 == i){
+				try{
+					NUM_OF_OBJECTS = Integer.parseInt(args[1]);
+				}catch(NumberFormatException e){
+					readFlag = false;
+					break;
+				}
+			}	
+		}
 
-    public static void main(String[] args) {
-	int NUM_OF_OBJECTS = Integer.parseInt(args[1]);
+		if (readFlag == false){
+			System.err.println("Error: Invalid command line inputs");
+			System.exit(1);
 
-
-	// FIXME: read the value of checkpointFile from the command line
-
-	ProxyCreator pc = new ProxyCreator();
-
-	// create an instance of StoreRestoreHandler (which implements
-	// the InvocationHandler
-	StoreRestoreHandler srHandler = new StoreRestoreHandler();
-	// create a proxy
-	StoreRestoreI cpointRef = (StoreRestoreI) pc.createProxy(
-								 new Class[] {
-								     StoreI.class, RestoreI.class
-								 },
-								 srHandler
-								 );
-
-	// FIXME: invoke a method on the handler instance to set the file name for checkpointFile and open the file
-	srHandler.setFilename(args[2]);
-	srHandler.openFile();
-	
-	MyAllTypesFirst myFirst;
-	MyAllTypesSecond  mySecond;
-
-	//Data structures for MyAllTypesFirst and MyAllTypesSecond
-	ArrayList<MyAllTypesFirst> myAllTypesFirstArr = new ArrayList<MyAllTypesFirst>();
-	ArrayList<MyAllTypesSecond> myAllTypesSecondArr = new ArrayList<MyAllTypesSecond>();
+		} else {
 		
 
-	// Use an if/switch to proceed according to the command line argument
-	// For deser, just deserliaze the input file into the data structure and then print the objects
-	// The code below is for "serdeser" mode
-	// For "serdeser" mode, both the serialize and deserialize functionality should be called.
+			// FIXME: read the value of checkpointFile from the command line
 
-	// create a data structure to store the objects being serialized
-        // NUM_OF_OBJECTS refers to the count for each of MyAllTypesFirst and MyAllTypesSecond
-	for (int i=0; i<NUM_OF_OBJECTS; i++) {
+			ProxyCreator pc = new ProxyCreator();
 
-	    // FIXME: create these object instances correctly using an explicit value constructor
-	    // use the index variable of this loop to change the values of the arguments to these constructors
-	    myFirst = new MyAllTypesFirst();
-	    mySecond = new MyAllTypesSecond();
+			// create an instance of StoreRestoreHandler (which implements
+			// the InvocationHandler
+			StoreRestoreHandler srHandler = new StoreRestoreHandler();
+			// create a proxy
+			StoreRestoreI cpointRef = (StoreRestoreI) pc.createProxy(
+										 new Class[] {
+											   StoreI.class, RestoreI.class
+										 },
+										 srHandler
+										 );
 
-	    // FIXME: store myFirst and mySecond in the data structure
-	    // FIXME: authCode --put in temporary one "666" to compile
-	    ((StoreI) cpointRef).writeObj(myFirst,666 ,"XML");
-	    ((StoreI) cpointRef).writeObj(mySecond,666 ,"XML");
+			// FIXME: invoke a method on the handler instance to set the file name for checkpointFile and open the file
+			srHandler.setFilename(args[2]);
+			srHandler.openFile();
+	
+			MyAllTypesFirst myFirst;
+			MyAllTypesSecond  mySecond;
 
+			//Data structures for MyAllTypesFirst and MyAllTypesSecond
+			ArrayList<MyAllTypesFirst> myAllTypesFirstArr = new ArrayList<MyAllTypesFirst>();
+			ArrayList<MyAllTypesSecond> myAllTypesSecondArr = new ArrayList<MyAllTypesSecond>();
+		
+
+			// Use an if/switch to proceed according to the command line argument
+			// For deser, just deserliaze the input file into the data structure and then print the objects
+			// The code below is for "serdeser" mode
+			// For "serdeser" mode, both the serialize and deserialize functionality should be called.
+
+			// create a data structure to store the objects being serialized
+			// NUM_OF_OBJECTS refers to the count for each of MyAllTypesFirst and MyAllTypesSecond
+			for (int i=0; i<NUM_OF_OBJECTS; i++) {
+
+				  // FIXME: create these object instances correctly using an explicit value constructor
+				  // use the index variable of this loop to change the values of the arguments to these constructors
+				  myFirst = new MyAllTypesFirst();
+				  mySecond = new MyAllTypesSecond();
+
+				  // FIXME: store myFirst and mySecond in the data structure
+				  // FIXME: authCode --put in temporary one "666" to compile
+				  ((StoreI) cpointRef).writeObj(myFirst,666 ,"XML");
+				  ((StoreI) cpointRef).writeObj(mySecond,666 ,"XML");
+
+			}
+
+			SerializableObject myRecordRet;
+
+			// create a data structure to store the returned ojects
+			for (int j=0; j<2*NUM_OF_OBJECTS; j++) {
+
+				  myRecordRet = ((RestoreI) cpointRef).readObj("XML");
+				  // FIXME: store myRecordRet in the vector
+			}
+
+			// FIXME: invoke a method on the handler to close the file (if it hasn't already been closed)
+
+			// FIXME: compare and confirm that the serialized and deserialzed objects are equal.
+			// The comparison should use the equals and hashCode methods. Note that hashCode
+			// is used for key-value based data structures
+		}
 	}
-
-	SerializableObject myRecordRet;
-
-	// create a data structure to store the returned ojects
-	for (int j=0; j<2*NUM_OF_OBJECTS; j++) {
-
-	    myRecordRet = ((RestoreI) cpointRef).readObj("XML");
-	    // FIXME: store myRecordRet in the vector
-	}
-
-	// FIXME: invoke a method on the handler to close the file (if it hasn't already been closed)
-
-	// FIXME: compare and confirm that the serialized and deserialzed objects are equal.
-	// The comparison should use the equals and hashCode methods. Note that hashCode
-	// is used for key-value based data structures
-
-    }
 }
--- genericCheckpointing/src/genericCheckpointing/xmlStoreRestore/StoreRestoreHandler.java
+++ genericCheckpointing/src/genericCheckpointing/xmlStoreRestore/StoreRestoreHandler.java
@@ -6,8 +6,9 @@ import genericCheckpointing.util.FileProcessor;
 
 public class StoreRestoreHandler implements InvocationHandler{
 	private String filename = null;
+	private FileProcessor fp = null;	
+
 	public Object invoke(Object proxy, Method m, Object[] args) throws Throwable{
-		
 		// FIXME: need to fix return statement and write this function.
 		return null;
 	}
@@ -21,6 +22,10 @@ public class StoreRestoreHandler implements InvocationHandler{
 	}
 	
 	public void openFile(){
-		FileProcessor fp = new FileProcessor(filename);
+		fp = new FileProcessor(filename);
+	}
+
+	public void closeFile(){
+		fp.closeFile();
 	}
 }
